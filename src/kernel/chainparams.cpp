1214edf05c6d81c0325e13f9a0f564cf483acf68
bitkini <team@bitkini.lol>
2025-06-29 17:22:12 +0200

    Genesis: Chain activated. Block 0 minted. Summer never ends. 🌊

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -1,3 +1,4 @@
+// Copyright (c) 2025 Bitkini Dev 0xMuted
 // Copyright (c) 2010 Satoshi Nakamoto
 // Copyright (c) 2009-present The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
